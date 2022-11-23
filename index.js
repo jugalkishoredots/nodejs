1b41be69b3939177860b88e893399c5d1bcd13fe
Jugal <jugal.kishore@dotsquares.com>
2022-11-23 08:31:28 +0530

    Update 5

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
-const path = require("path");
+const express           = require('express');
+const expressLayouts    = require('express-ejs-layouts');
+// const flash             = require('connect-flash');
+const { flash } = require('express-flash-message');
+const session           = require('express-session');
+const bodyParser        = require('body-parser');
+const app               = express();
+const port              = 3000;
+const path              = require("path");
 
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
@@ -19,6 +22,19 @@ app.set("views", path.join(__dirname, "src/views"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use('/public', express.static(path.join(__dirname, 'public')));
+// express-session
+app.use(
+    session({
+      secret: 'secret',
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+        // secure: true, // becareful set this option, check here: https://www.npmjs.com/package/express-session#cookiesecure. In local, if you set this to true, you won't receive flash as you are using `http` in local, but http is not secure
+      },
+    })
+  );
+app.use(flash());
 
 
 require("./src/routes/route")(app);
--- src/controllers/Admin/AdminUserController.js
+++ src/controllers/Admin/AdminUserController.js
@@ -1,42 +1,59 @@
 module.exports = {
-    index: (req, res) => {
-       // res.render("users/index", {data: "Dynamic Data"});
-       res.render("web/index", {data: "Dynamic Data",  layout: './layouts/sidebar'});
-    },
-    login: (req, res) => {
-       // res.render("users/index", {data: "Dynamic Data"});
-      //  res.render("web/login", {data: "Dynamic Data"});
-       res.render("admin/admin/login", { layout: "./layouts/admin/login_layout" });
-    },
-    loginPost: (req, res) => {
-      console.log(req.body)
-       // res.render("users/index", {data: "Dynamic Data"});
-       res.redirect("/admin/dashboard");
+  index: (req, res) => {
+    // res.render("users/index", {data: "Dynamic Data"});
+    res.render("web/index", {
+      data: "Dynamic Data",
+      layout: "./layouts/sidebar",
+    });
+  },
+  login: (req, res) => {
+    // res.render("users/index", {data: "Dynamic Data"});
+    //  res.render("web/login", {data: "Dynamic Data"});
+    res.render("admin/admin/login", {
+      title: ":: Admin Login ::",
+      data: [],
+      message: req.flash("error"),
+      layout: "./layouts/admin/login_layout",
+    });
+  },
+  loginPost: (req, res) => {
+    console.log(req.body);
+    // res.render("users/index", {data: "Dynamic Data"});
+    res.redirect("/admin/dashboard");
     //    res.render("web/index", {data: "Dynamic Data"});
-    },
-    register: (req, res) => {
-       // res.render("users/index", {data: "Dynamic Data"});
-       res.render("web/register", {data: "Dynamic Data"});
-    },
-    registerPost: (req, res) => {
-       // res.render("users/index", {data: "Dynamic Data"});
-       res.render("web/index", {data: "Dynamic Data"});
-    },
-    dashboard: (req, res) => {
-       // res.render("users/index", {data: "Dynamic Data"});
-       res.render("admin/dashboard/AdminDashboard", { layout: './layouts/admin/admin_layout', data: "Dynamic Data"});
-    },
-    myProfile: (req, res) => {
-       // res.render("users/index", {data: "Dynamic Data"});
-       res.render("admin/admin/edit_profile", { layout: './layouts/admin/admin_layout', data: "Dynamic Data"});
-    },
-    settings: (req, res) => {
-       // res.render("users/index", {data: "Dynamic Data"});
-       res.render("admin/admin/setting", { layout: './layouts/admin/admin_layout', data: "Dynamic Data"});
-    },
-    logout: (req, res) => {
-       // res.render("users/index", {data: "Dynamic Data"});
-       res.redirect("/admin");
-      //  res.render("admin/login/login", { layout: './layouts/admin/admin_layout', data: "Dynamic Data"});
-    }
-}
+  },
+  register: (req, res) => {
+    // res.render("users/index", {data: "Dynamic Data"});
+    res.render("web/register", { data: "Dynamic Data" });
+  },
+  registerPost: (req, res) => {
+    // res.render("users/index", {data: "Dynamic Data"});
+    res.render("web/index", { data: "Dynamic Data" });
+  },
+  dashboard: (req, res) => {
+    // res.render("users/index", {data: "Dynamic Data"});
+    res.render("admin/dashboard/AdminDashboard", {
+      layout: "./layouts/admin/admin_layout",
+      data: "Dynamic Data",
+    });
+  },
+  myProfile: (req, res) => {
+    // res.render("users/index", {data: "Dynamic Data"});
+    res.render("admin/admin/edit_profile", {
+      layout: "./layouts/admin/admin_layout",
+      data: "Dynamic Data",
+    });
+  },
+  settings: (req, res) => {
+    // res.render("users/index", {data: "Dynamic Data"});
+    res.render("admin/admin/setting", {
+      layout: "./layouts/admin/admin_layout",
+      data: "Dynamic Data",
+    });
+  },
+  logout: (req, res) => {
+    //  req.session.user = {};
+    req.flash("success", "User successfully logout!");
+    res.redirect("/admin");
+  },
+};
