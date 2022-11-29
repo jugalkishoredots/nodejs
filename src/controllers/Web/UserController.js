9bae0da5daf3c568bfe4f2ce8bf94b7166bcc0b9
Jugal <jugal.kishore@dotsquares.com>
2022-11-29 12:31:22 +0530

    Update UserController.js
    Email sent successfully

--- src/controllers/Web/UserController.js
+++ src/controllers/Web/UserController.js
@@ -66,31 +66,56 @@ module.exports = {
   },
   forgotPasswordMail: async (req, res) => {
     console.log(req.body);
-    let testAccount = await nodemailer.createTestAccount();
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, // true for 465, false for other ports
+    // let testAccount = await nodemailer.createTestAccount();
+    // // create reusable transporter object using the default SMTP transport
+    // let transporter = nodemailer.createTransport({
+    //   host: "smtp.ethereal.email",
+    //   port: 587,
+    //   secure: false, // true for 465, false for other ports
+    //   auth: {
+    //     user: testAccount.user, // generated ethereal user
+    //     pass: testAccount.pass, // generated ethereal password
+    //   },
+    // });
+    // // send mail with defined transport object
+    // let info = await transporter.sendMail({
+    //   from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    //   to: "jugalds@yopmail.com", // list of receivers
+    //   subject: "Hello ✔", // Subject line
+    //   text: "Hello world?", // plain text body
+    //   html: "<b>Hello world?</b>", // html body
+    // });
+
+    let testAccount = await nodemailer.createTransport({
+      host: "mail.24livehost.com",
+      port: 25,
+      secure: false, // upgrade later with STARTTLS
       auth: {
-        user: testAccount.user, // generated ethereal user
-        pass: testAccount.pass, // generated ethereal password
+        user: "ds27@24livehost.com",
+        pass: "LFo3h1r5zxdjpq",
+      },
+      tls: {
+        // do not fail on invalid certs
+        rejectUnauthorized: false,
       },
     });
+
     // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    let info = testAccount.sendMail({
+      from: '"Dots" <ds27@24livehost.com>', // sender address
       to: "jugalds@yopmail.com", // list of receivers
       subject: "Hello ✔", // Subject line
       text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
+      // html: "<b>Hello world?</b>", // html body
     });
 
-    console.log("Message sent: %s", info.messageId);
+    console.log("Message sent: %s", testAccount);
+    req.flash("success", "Forgot Password Email sent on your mail ID !");
+            res.redirect("/login");
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
     // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   },
 };
